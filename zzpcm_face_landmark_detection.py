14e5707cc8c7df91c00ab210c445dad4eb7eaf73
Park Chan-Min <walkinpcm@gmail.com>
2016-05-16 17:53:24 +0900

    Update zzpcm_face_landmark_detection.py

--- zzpcm_face_landmark_detection.py
+++ zzpcm_face_landmark_detection.py
@@ -82,17 +82,17 @@ for i in range(10):
     # second argument indicates that we should upsample the image 1 time. This
     # will make everything bigger and allow us to detect more faces.
     
-    #dets = detector(img, 1)
-    #print("Number of faces detected: {}".format(len(dets)))
-    #for k, d in enumerate(dets):
-    #    print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(
-    #        k, d.left(), d.top(), d.right(), d.bottom()))
+    dets = detector(img, 1)
+    print("Number of faces detected: {}".format(len(dets)))
+    for k, d in enumerate(dets):
+        print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(
+            k, d.left(), d.top(), d.right(), d.bottom()))
         # Get the landmarks/parts for the face in box d.
-    #    shape = predictor(img, d)
-    #    print("Part 0: {}, Part 1: {}, Part 67: {} ...".format(shape.part(0),
-    #                                              shape.part(1),shape.part(67)))
+        shape = predictor(img, d)
+        print("Part 0: {}, Part 1: {}, Part 67: {} ...".format(shape.part(0),
+                                                  shape.part(1),shape.part(67)))
         # Draw the face landmarks on the screen.
-    #    win.add_overlay(shape)
+        win.add_overlay(shape)
 
     #win.add_overlay(dets)
     #dlib.hit_enter_to_continue()
