3e44f3b8e305a37530ce62848d62e98f48a25189
Park Chan-Min <walkinpcm@gmail.com>
2016-05-18 10:36:04 +0900

    Update zzpcm_face_landmark_detection.py

--- zzpcm_face_landmark_detection.py
+++ zzpcm_face_landmark_detection.py
@@ -71,32 +71,32 @@ predictor = dlib.shape_predictor(predictor_path)
 win = dlib.image_window()
 
 for f in glob.glob(os.path.join(faces_folder_path, "zzpcm_faceimg.jpg")):
-#for i in range(10):
-    #print("Processing file: {}".format(f))
+    for i in range(10):
+        #print("Processing file: {}".format(f))
     
     
-    ret, img = cam.read()
-    cv2.imwrite(faces_folder_path+"/zzpcm_faceimg.jpg")
-    img = io.imread(f)
-    win.clear_overlay()
-    win.set_image(img)
+        ret, img = cam.read()
+        cv2.imwrite(faces_folder_path+"/zzpcm_faceimg.jpg")
+        img = io.imread(f)
+        win.clear_overlay()
+        win.set_image(img)
 
-    # Ask the detector to find the bounding boxes of each face. The 1 in the
-    # second argument indicates that we should upsample the image 1 time. This
-    # will make everything bigger and allow us to detect more faces.
+        # Ask the detector to find the bounding boxes of each face. The 1 in the
+        # second argument indicates that we should upsample the image 1 time. This
+        # will make everything bigger and allow us to detect more faces.
     
-    dets = detector(img, 1)
-    print("Number of faces detected: {}".format(len(dets)))
-    for k, d in enumerate(dets):
-        print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(
-            k, d.left(), d.top(), d.right(), d.bottom()))
-        # Get the landmarks/parts for the face in box d.
-        shape = predictor(img, d)
-        #there is 68 points in shape variable.(part(0)~part(67))
-        print("Part 0: {}, Part 1: {}, Part 67: {} ...".format(shape.part(0),
-                                                  shape.part(1),shape.part(67)))
-        # Draw the face landmarks on the screen.
-        win.add_overlay(shape)
+        dets = detector(img, 1)
+        print("Number of faces detected: {}".format(len(dets)))
+        for k, d in enumerate(dets):
+            print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(
+                k, d.left(), d.top(), d.right(), d.bottom()))
+            # Get the landmarks/parts for the face in box d.
+            shape = predictor(img, d)
+            #there is 68 points in shape variable.(part(0)~part(67))
+            print("Part 0: {}, Part 1: {}, Part 67: {} ...".format(shape.part(0),
+                                                     shape.part(1),shape.part(67)))
+            # Draw the face landmarks on the screen.
+            win.add_overlay(shape)
 
-    #win.add_overlay(dets)
-    dlib.hit_enter_to_continue()
+        #win.add_overlay(dets)
+        dlib.hit_enter_to_continue()
