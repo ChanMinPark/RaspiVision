b015c5f309b045f3b17d8160333a2b201a96731b
Park Chan-Min <walkinpcm@gmail.com>
2016-05-18 11:28:57 +0900

    Update zzpcm_face_landmark_detection.py

--- zzpcm_face_landmark_detection.py
+++ zzpcm_face_landmark_detection.py
@@ -46,62 +46,63 @@ import sys
 import os
 import dlib
 import glob
-from skimage import io
+#from skimage import io
 import cv2
 
-if len(sys.argv) != 3:
+if len(sys.argv) != 2:
     print(
         "Give the path to the trained shape predictor model as the first "
-        "argument and then the directory containing the facial images.\n"
+        "argument.\n"
         "For example, if you are in the python_examples folder then "
         "execute this program by running:\n"
-        "    python face_landmark_detection.py shape_predictor_68_face_landmarks.dat ../examples/faces\n"
+        "    python face_landmark_detection.py shape_predictor_68_face_landmarks.dat\n"
         "You can download a trained facial shape predictor from:\n"
         "    http://sourceforge.net/projects/dclib/files/dlib/v18.10/shape_predictor_68_face_landmarks.dat.bz2")
     exit()
 
 predictor_path = sys.argv[1]
-faces_folder_path = sys.argv[2]
+#faces_folder_path = sys.argv[2]
 
 cam = cv2.VideoCapture(0)
-cam.set(cv2.CAP_PROP_FPS, 2)
+cam.set(cv2.CAP_PROP_FPS, 3)
 
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(predictor_path)
 win = dlib.image_window()
 
 ret, frame = cam.read()
-cv2.imwrite(faces_folder_path+"/zzpcm_faceimg.jpg", frame)
+#cv2.imwrite(faces_folder_path+"/zzpcm_faceimg.jpg", frame)
 
-for f in glob.glob(os.path.join(faces_folder_path, "zzpcm_faceimg.jpg")):
-    for i in range(10):
-        #print("Processing file: {}".format(f))
-        
-        #cv2.VideoCapture.release()
-        cam = cv2.VideoCapture(0)
-        cam.set(cv2.CAP_PROP_FPS, 2)
-        ret, frame = cam.read()
-        cv2.imwrite(faces_folder_path+"/zzpcm_faceimg.jpg", frame)
-        img = io.imread(f)
-        win.clear_overlay()
-        win.set_image(img)
-
-        # Ask the detector to find the bounding boxes of each face. The 1 in the
-        # second argument indicates that we should upsample the image 1 time. This
-        # will make everything bigger and allow us to detect more faces.
+#for f in glob.glob(os.path.join(faces_folder_path, "zzpcm_faceimg.jpg")):
+for i in range(10):
+    #print("Processing file: {}".format(f))
     
-        dets = detector(img, 1)
-        print("Number of faces detected: {}".format(len(dets)))
-        for k, d in enumerate(dets):
-            print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(
-                k, d.left(), d.top(), d.right(), d.bottom()))
-            # Get the landmarks/parts for the face in box d.
-            shape = predictor(img, d)
-            #there is 68 points in shape variable.(part(0)~part(67))
-            print("Part 0: {}, Part 1: {}, Part 67: {} ...".format(shape.part(0),
-                                                     shape.part(1),shape.part(67)))
-            # Draw the face landmarks on the screen.
-            win.add_overlay(shape)
+    #cv2.VideoCapture.release()
+    cam = cv2.VideoCapture(0)
+    cam.set(cv2.CAP_PROP_FPS, 3)
+    ret, frame = cam.read()
+    img = frame
+    #cv2.imwrite(faces_folder_path+"/zzpcm_faceimg.jpg", frame)
+    #img = io.imread(f)
+    win.clear_overlay()
+    win.set_image(img)
 
-        #win.add_overlay(dets)
-        dlib.hit_enter_to_continue()
+    # Ask the detector to find the bounding boxes of each face. The 1 in the
+    # second argument indicates that we should upsample the image 1 time. This
+    # will make everything bigger and allow us to detect more faces.
+    
+    dets = detector(img, 1)
+    print("Number of faces detected: {}".format(len(dets)))
+    for k, d in enumerate(dets):
+        print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(
+            k, d.left(), d.top(), d.right(), d.bottom()))
+        # Get the landmarks/parts for the face in box d.
+        shape = predictor(img, d)
+        #there is 68 points in shape variable.(part(0)~part(67))
+        print("Part 0: {}, Part 1: {}, Part 67: {} ...".format(shape.part(0),
+                                                 shape.part(1),shape.part(67)))
+        # Draw the face landmarks on the screen.
+        win.add_overlay(shape)
+
+    #win.add_overlay(dets)
+    dlib.hit_enter_to_continue()
