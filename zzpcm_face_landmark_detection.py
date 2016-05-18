ce43d425ae5b806038c97edcf79dc2fc41553970
Park Chan-Min <walkinpcm@gmail.com>
2016-05-18 13:18:13 +0900

    Update zzpcm_face_landmark_detection.py

--- zzpcm_face_landmark_detection.py
+++ zzpcm_face_landmark_detection.py
@@ -81,6 +81,7 @@ for i in range(10):
     cam = cv2.VideoCapture(0)
     cam.set(cv2.CAP_PROP_FPS, 7)
     ret, frame = cam.read()
+    img = frame
     #cv2.imwrite(faces_folder_path+"/zzpcm_faceimg.jpg", frame)
     #img = io.imread(f)
     win.clear_overlay()
