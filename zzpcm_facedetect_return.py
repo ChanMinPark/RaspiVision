cf7229c1f6917e2d397d408d93193654f338ee11
Park Chan-Min <walkinpcm@gmail.com>
2016-05-18 17:27:32 +0900

    Update zzpcm_facedetect_return.py

--- zzpcm_facedetect_return.py
+++ zzpcm_facedetect_return.py
@@ -5,16 +5,53 @@ import sys, getopt
 import cv2
 import numpy as np
 
-from video import create_capture
+#from video import create_capture
 from common import clock, draw_str
 
 help_message = '''
 USAGE: zzpcm_facedetect_return.py --mode (local/network) [--sdir <save_dir>] [--sname <save_name>]
 '''
 
+def detect(img, cascade):
+    rects = cascade.detectMultiScale(img, scaleFactor=1.3, minNeighbors=4, minSize=(30, 30),
+                                     flags=cv2.CASCADE_SCALE_IMAGE)
+    if len(rects) == 0:
+        return []
+    rects[:,2:] += rects[:,:2]
+    return rects
 
-def localMode():
+def facedetect(img):
   #To do
+  resultflag = 0
+  
+  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  gray = cv2.equalizeHist(gray)
+  cascade = "../../data/haarcascades/haarcascade_frontalface_alt.xml"
+  
+  rects = detect(gray, cascade)
+  if len(rects)!=0:
+    resultflag=1
+  
+  return resultflag
+
+def saveImage(img, s_dir, s_name):
+  #To do
+
+def localMode(s_dir, s_name):
+  #To do
+  cam = cv2.VideoCapture(0)
+  cam.set(cv2.CAP_PROP_FPS, 7)
+  ret, img = cam.read()
+  
+  result = facedetect(img)
+  
+  if result==1:
+    if s_dir[-1:] != "/":
+      s_dir = s_dir+'/'
+    print("Save the Image.")
+    cv2.imwrite(s_dir+s_name, img)
+  else:
+    print("There is no face. Do nothing.")
 
 def networkMode():
   #To do
@@ -29,7 +66,7 @@ if __name == '__main__':
   save_name = args.get('--sname', 'tempImage.jpg')
   
   if running_mode =='local':
-    localMode()
+    localMode(save_dir, save_name)
   elif running_mode == 'network':
     networkMode()
   else:
