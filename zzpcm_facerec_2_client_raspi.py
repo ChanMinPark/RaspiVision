4b61e2b9cdc6129d704a59b34b39a7dcd7b6dec8
Park Chan-Min <walkinpcm@gmail.com>
2016-05-26 15:02:21 +0900

    Add files via upload

---
+++ zzpcm_facerec_2_client_raspi.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+
+import sys
+import cv2
+import numpy as np
+import socket
+import time
+
+
+face_cascade = cv2.CascadeClassifier("[change Dir]/haarcascades/haarcascade_frontalface_alt.xml")
+
+
+def recv_data(sock, count):
+    buf = b''
+    while count:
+        newbuf = sock.recv(count)
+        if not newbuf: return None
+        buf += newbuf
+        count -= len(newbuf)
+    return buf
+
+
+def register_proc(name):
+    #To do
+    print("Begin register process.")
+
+    # 'ip'and 'port' must be changed depend on your System.
+    ip = '192.168.0.72'
+    port = 10100  #My Receive port
+
+    ss = socket.socket()
+    ss.connect(('192.168.0.67', 10200))
+  
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((ip, port))
+    s.listen(True)
+
+    cam = cv2.VideoCapture(0)
+    cam.set(cv2.CAP_PROP_FPS, 7)
+
+    for count in range(10):
+        try:
+            ret, frame = cam.read()
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray, 1.2, 3)
+            for (x,y,w,h) in faces:
+                resized_image = cv2.resize(frame[y:y+h,x:x+w], (256, 256))
+                
+                encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),90]
+                result, imgencode = cv2.imencode('.jpg', resized_image, encode_param)
+                data = numpy.array(imgencode)
+                stringData = data.tostring()
+                metaMsg = 'r/'+str(port)+str(len(stringData)).ljust(16)+name.ljust(10)
+                
+                ss.send(metaMsg)
+                ss.send(stringData)
+            
+            time.sleep(1)
+        except KeyboardInterrupt:
+            s.close()
+            ss.close()
+            sys.exit()
+            
+    print "Register process is end."
+    s.close()
+    ss.close()
+
+
+def operation_proc():
+    #To do
+    print("Begin register process.")
+
+    # 'ip'and 'port' must be changed depend on your System.
+    ip = '192.168.0.72'
+    port = 10100  #My Receive port
+
+    ss = socket.socket()
+    ss.connect(('192.168.0.67', 10200))
+  
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((ip, port))
+    s.listen(True)
+
+    cam = cv2.VideoCapture(0)
+    cam.set(cv2.CAP_PROP_FPS, 7)
+
+    resultData = "Unknown"
+    for count in range(1):
+        try:
+            ret, frame = cam.read()
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray, 1.2, 3)
+            for (x,y,w,h) in faces:
+                resized_image = cv2.resize(frame[y:y+h,x:x+w], (256, 256))
+                
+                encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),90]
+                result, imgencode = cv2.imencode('.jpg', resized_image, encode_param)
+                data = numpy.array(imgencode)
+                stringData = data.tostring()
+                name="none"
+                metaMsg = 'o/'+str(port)+str(len(stringData)).ljust(16)+name.ljust(10)
+                
+                ss.send(metaMsg)
+                ss.send(stringData)
+
+                rs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                rs.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                rs.bind((myip, myport))
+                rs.listen(True)
+
+                con, saddr = rs.accept()
+                resultData = recv_data(con, 10)
+                print resultData
+                
+            #time.sleep(1)
+        except KeyboardInterrupt:
+            s.close()
+            ss.close()
+            sys.exit()
+            
+    print "Register process is end."
+    s.close()
+    ss.close()
+            
+
+if __name__ == '__main__':
+    menu = int(raw_input('Select main menu ( 1: Register your face, 2: Start ) : '))
+
+    if menu == 1:
+        name = int(raw_input('Enter your name : '))
+        if len(name) > 10:
+            name = name[:10]
+        register_proc(name)
+    elif menu == 2:
+        operation_proc()
+    else:
+        print "Incorrect menu. Terminate program."
+
+    
+  
---
+++ zzpcm_facerec_2_server.py
@@ -0,0 +1,145 @@
+#!/usr/bin/python
+from facerec.feature import Fisherfaces
+from facerec.classifier import NearestNeighbor
+from facerec.model import PredictableModel
+from PIL import Image
+import numpy as np
+import socket
+import sys, os
+import time
+import cv2
+import multiprocessing
+import random
+
+#'path' must be changed depend on your System.
+path = '/home/pcm/drvs/facedatabase/'
+
+#Load Image in Face Database
+def read_images(path, sz=(256,256)):
+    """Reads the images in a given folder, resizes images on the fly if size is given.
+    Args:
+        path: Path to a folder with subfolders representing the subjects (persons).
+        sz: A tuple with the size Resizes 
+    Returns:
+        A list [X,y]
+            X: The images, which is a Python list of numpy arrays.
+            y: The corresponding labels (the unique number of the subject, person) in a Python list.
+    """
+    c = 0
+    X,y = [], []
+    folder_names = []
+    
+    for dirname, dirnames, filenames in os.walk(path):
+        for subdirname in dirnames:
+            folder_names.append(subdirname)
+            subject_path = os.path.join(dirname, subdirname)
+            for filename in os.listdir(subject_path):
+                try:
+                    im = cv2.imread(os.path.join(subject_path, filename), cv2.IMREAD_GRAYSCALE)
+                    
+                    # resize to given size (if given)
+                    if (sz is not None):
+                        im = cv2.resize(im, sz)
+                    X.append(np.asarray(im, dtype=np.uint8))
+                    y.append(c)
+                except IOError, (errno, strerror):
+                    print "I/O error({0}): {1}".format(errno, strerror)
+                except:
+                    print "Unexpected error:", sys.exc_info()[0]
+                    raise
+            c = c+1
+    return [X,y,folder_names]
+
+def recv_Data(sock, count):
+    buf = b''
+    while count:
+        newbuf = sock.recv(count)
+        if not newbuf: return None
+        buf += newbuf
+        count -= len(newbuf)
+    return buf
+
+def save_FaceDB(name, img):
+    if not os.path.exists(path+name): os.makedirs(path+name)
+    cv2.imwrite(path+name+'/'+str(random.randrange(100,1000)),img)
+    
+def checkFace(origin_img):
+    #To do
+    model = PredictableModel(Fisherfaces(), NearestNeighbor())
+    #face_cascade = cv2.CascadeClassifier("/home/pi/Desktop/opencv-3.0.0/data/haarcascades/haarcascade_frontalface_alt.xml")
+    
+    result_name = 'unknown'
+    
+    [X,y,subject_names] = read_images(path)
+    list_of_labels = list(xrange(max(y)+1))
+    subject_dictionary = dict(zip(list_of_labels, subject_names))
+    model.compute(X,y)
+
+    #gray = cv2.cvtColor(origin_img, cv2.COLOR_BGR2GRAY)
+    #faces = face_cascade.detectMultiScale(gray, 1.2, 3)
+
+    #for (x,y,w,h) in faces:        
+        #sampleImage = gray[y:y+h, x:x+w]
+        #sampleImage = cv2.resize(sampleImage, (256,256))
+    sampleImage = cv2.resize(origin_img, (256,256))
+
+        
+    [ predicted_label, generic_classifier_output] = model.predict(sampleImage)
+    print [ predicted_label, generic_classifier_output]
+        
+    if int(generic_classifier_output['distances']) <=  700:
+        result_name = str(subject_dictionary[predicted_label])
+
+    return result_name
+
+
+
+#TCP_IP = 'localhost'  #remote ip
+#TCP_PORT = 10100      #remote port
+
+#'myip'and 'myport' must be changed depend on your System.
+myip = '192.168.0.67'
+myport = 10200
+
+rs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+rs.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+rs.bind((myip, myport))
+rs.listen(True)
+
+#ss.send('request/'+str(myport))
+while True:
+    try:
+        conn, addr = rs.accept()
+
+
+        for count in range(10):
+            metaData = recv_Data(conn,35)
+
+            menu = metaData[:1]
+            client_port = int(metaData[2:7])
+            length = int(metaData[8:24])
+            user_name = metaData[25:]
+
+            stringData = recv_Data(conn, length)
+            data = numpy.fromstring(stringData, dtype='uint8')
+            decimg=cv2.imdecode(data,1)
+            
+            if menu=='r':
+                print "Request is accepted : Register"
+                save_FaceDB(user_name, decimg)
+                print "Save "+user_name+"image #"+str(count+1)
+            else:
+                print "Request is accepted : Operation"
+                who = checkFace(decimg)
+                print("Result Face : "+who)
+                ss = socket.socket()
+                ss.connect((addr[0], client_port))
+                ss.send(who.ljust(10))
+                ss.close()
+                break
+    
+    except KeyboardInterrupt:
+        rs.close()
+        sys.exit()
+
+rs.close()
